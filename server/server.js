ce131e8e51307521dc189397161c9dc44fd489ba
Paridhi Mittal <paridhimittal0210@gmail.com>
2025-09-22 15:29:46 +0530

    server.js file added

---
+++ server/server.js
@@ -0,0 +1,24 @@
+import 'dotenv/config'
+import express from 'express'
+import cors from 'cors'
+import connectDB from './configs/mongodb.js'
+
+const PORT = process.env.PORT || 4000
+const app = express()
+
+//middlewares
+app.use(express.json())
+app.use(cors())
+
+//database connection
+await connectDB()
+
+
+app.get('/', (req, res) => {
+  res.send('API WORKING')
+})
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`)
+})
+
